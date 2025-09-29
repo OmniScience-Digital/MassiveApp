beeca81825856e19b8bf0b5a0a86119d1f118a9c
ThabisoBlessed <thabiso@omniscience.digital>
2025-09-29 19:49:08 +0200

    changing url to prod from baseurl

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,25 +3,25 @@ import { defineAuth, secret } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    externalProviders: {
-      google: {
-        clientId: secret("GOOGLE_CLIENT_ID"),
-        clientSecret: secret("GOOGLE_SECRET"),
-        scopes: ["profile", "email"],
-        attributeMapping: {
-          email: "email",
-        },
-      },
-      callbackUrls: [
-        "http://localhost:3000/landing",
-        "https://main.d1d7dmip95q0zl.amplifyapp.com/landing",
-        "https://test.d1d7dmip95q0zl.amplifyapp.com/landing",
-      ],
-      logoutUrls: [
-        "http://localhost:3000",
-        "https://main.d1d7dmip95q0zl.amplifyapp.com",
-        "https://test.d1d7dmip95q0zl.amplifyapp.com",
-      ],
-    },
+    // externalProviders: {
+    //   google: {
+    //     clientId: secret("GOOGLE_CLIENT_ID"),
+    //     clientSecret: secret("GOOGLE_SECRET"),
+    //     scopes: ["profile", "email"],
+    //     attributeMapping: {
+    //       email: "email",
+    //     },
+    //   },
+    //   callbackUrls: [
+    //     "http://localhost:3000/landing",
+    //     "https://main.d1d7dmip95q0zl.amplifyapp.com/landing",
+    //     "https://test.d1d7dmip95q0zl.amplifyapp.com/landing",
+    //   ],
+    //   logoutUrls: [
+    //     "http://localhost:3000",
+    //     "https://main.d1d7dmip95q0zl.amplifyapp.com",
+    //     "https://test.d1d7dmip95q0zl.amplifyapp.com",
+    //   ],
+    // },
   },
 });
